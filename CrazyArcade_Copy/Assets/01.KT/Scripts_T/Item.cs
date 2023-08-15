31b47f3182ed7d90a2f34624e592983b0c265f2d
BaeKyungTaek <kyungtaek0204@gmail.com>
2023-08-15 20:23:03 +0900

    feat : "Item follow MoveBox"
    - 아이템 프리펩에 Item스크립트를 넣어, MoveBox와 Trigger되어있을경우 해당 박스를 따라가도록 함

--- CrazyArcade_Copy/Assets/01.KT/Scripts_T/Item.cs
+++ CrazyArcade_Copy/Assets/01.KT/Scripts_T/Item.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PassiveItem : MonoBehaviour
+public class Item : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         //처음생성시에는 비활성화 했다가, 특정시점에 활성화 필요(ex_ 타일이 삭제 or 비행기에서 떨어질때)
         
-        gameObject.SetActive(false);
+        //gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -17,6 +17,13 @@ public class PassiveItem : MonoBehaviour
     {
         
     }
-
     //TODO 타일 삭제 시 gameObject.SetActive(true);
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "MoveBox")
+        {
+            transform.position = collision.transform.position;
+        }
+    }
 }
