cbb484220c8ca7590d92d0f2279c2f406ed7cb20
DESKTOP-DSTMQ36\KGA <meeme@outlook.kr>
2023-08-21 21:10:15 +0900

    add : "Switch player Object Status"
    Switch Object's SetActive status to true according to player entry order

--- CrazyArcade_Copy/Assets/01.KT/Scripts_T/ReadySceneManager.cs
+++ CrazyArcade_Copy/Assets/01.KT/Scripts_T/ReadySceneManager.cs
@@ -9,24 +9,44 @@ public class ReadySceneManager : MonoBehaviourPunCallbacks
 {
     public Button startButton;
 
+    private int playerNum = 0;
     private GameObject[] readyPlayerPositions;
+
     private void Awake()
     {
         // 마스터 클라이언트의 씬 자동 동기화 옵션
         // 룸에 입장한 다른 접속 유저들에게도 마스터 클라이언트의 씬을 자동으로 로딩 해주기 위해
         PhotonNetwork.AutomaticallySyncScene = true;
 
+
+        readyPlayerPositions = new GameObject[8];
         // 하이어라키창의 캐릭터 입장 위치들을 배열에 저장
-        readyPlayerPositions = GameObject.FindGameObjectsWithTag("ReadyPlayerPosition");
+        //readyPlayerPositions = GameObject.FindGameObjectsWithTag("ReadyPlayerPosition");
+        readyPlayerPositions[0] = GameObject.Find("ReadyPlayerPosition").transform.GetChild(0).gameObject;
+        Debug.Log(readyPlayerPositions[0].name);
+        Debug.Log(GameObject.Find("FirstPlayer").transform.root.GetChild(0).name);
+
+        // GameObject.Find(이름) -> Scene 하위의 모든 오브젝트 중에서 일치하는 이름의 GameObject 를 가져옴
+        // gameObject.transform.Find 또는 gameObject.transform.GetChild(인덱스 번호) -> 'gameObject' 이 오브젝트를 부모로 삼고
+        // 그 하위에 있는 자식 오브젝트를 인덱스 번호에 맞게 가져옴
+        // gameObject.transform.parent.parent -> 부모-부모를 부름
+        // gameObject.transform.root.getChild(인덱스번호) -> 씬을 제외하고 가장 최상의 오브젝트의 인덱스번호 번째 오브젝트를 부름
+
 
         // 캐릭터 입장시 순서에 맞춰 캐릭터 오브젝트 true
         OnReadyScene();
 
     }
 
+
+    // 캐릭터 입장 순서에 맞춰서 오브젝트 나타내는
     public void OnReadyScene()
     {
+        Debug.Log($"{readyPlayerPositions.Length}");
+        Debug.Log($"{playerNum}");
 
+        readyPlayerPositions[playerNum].SetActive(false);
+        playerNum++;
     }
     public void OnStartButtonClick()
     {
